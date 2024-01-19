ca3738d3d7ed42578cb86887745754729656418f
diegotanel <dtanel@gmail.com>
2024-01-19 10:30:45 -0300

    Agregar archivos de proyecto.

---
+++ DevelopmentChallenge.Data/Classes/program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentChallenge.Data.Classes
+{
+    public static class program
+    {
+        static public void Main(String[] args)
+        {
+            Console.WriteLine("Main Method");
+        }
+    }
+
+ 
+}
